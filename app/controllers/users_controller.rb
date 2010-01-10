4272930cba13b7aa274829c3adb5b64b55a39982
Stefano Costantini <stefano.costantini@gmail.com>
2010-01-10 23:48:06 +0000

    fixed new user nil error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,24 @@ class UsersController < ApplicationController
     		@goal.duration = @goal.hours*3600 + @goal.minutes*60 + @goal.seconds 
     		@goal.avgspeed = ((@goal.distance/@goal.duration)*3600).round(1)
     		@goal.save
+    		
+    	#create default run#1	
+    		@run = current_user.runs.new #(params[:goal])
+       		@run.distance = 8
+    		@run.hours = 0
+    		@run.minutes = 40
+    		@run.seconds = 0
+    		@run.note = 'This is a sample entry, you can edit it by clicking on the all fields'
+    		@run.duration = @run.hours*3600 + @run.minutes*60 + @run.seconds 
+    		@run.avgspeed = ((@run.distance/@run.duration)*3600).round(1)
+    		
+    		@run.distance_comparison = (((@run.distance-@goal.distance)/@goal.distance)*100).round(1)
+    		#@run.duration_comparison = (((@run.duration-@goal.duration)/@goal.duration)*100).round(1)
+    		@run.avgspeed_comparison = (((@run.avgspeed-@goal.avgspeed)/@goal.avgspeed)*100).round(1)
+    		@run.total_comparison = ((@run.distance_comparison+@run.avgspeed_comparison)/2).round(1)
+    		    		
+    		@run.save
+    		 	
      else
       render :action => 'new'
     end
